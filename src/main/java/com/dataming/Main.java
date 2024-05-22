8385ebab728039487ebd6e69a5e17500a59021f6
Toxica20002 <dinhquoclam02@gmail.com>
2024-05-22 10:25:33 +0700

    update print table

--- src/main/java/com/dataming/Main.java
+++ src/main/java/com/dataming/Main.java
@@ -2,6 +2,9 @@ package com.dataming;
 
 import com.dataming.pre_processing.DataProcess;
 import com.github.freva.asciitable.AsciiTable;
+import com.github.freva.asciitable.Column;
+import com.github.freva.asciitable.ColumnData;
+import com.github.freva.asciitable.HorizontalAlign;
 import weka.classifiers.Classifier;
 import weka.classifiers.evaluation.Evaluation;
 import weka.classifiers.functions.LinearRegression;
@@ -17,12 +20,14 @@ import weka.classifiers.trees.RandomTree;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        String csvFilePath= "./raw_data/wind_dataset_cleaned.csv";
-        String preprocessedArffFilePath= "./src/main/resources/clean_data/wind_dataset_preprocessed.arff";
+        String csvFilePath = "./raw_data/wind_dataset_cleaned.csv";
+        String preprocessedArffFilePath = "./src/main/resources/clean_data/wind_dataset_preprocessed.arff";
         DataProcess.save_csv2arff(csvFilePath, preprocessedArffFilePath);
 
         DataSource source = new DataSource("./src/main/resources/clean_data/wind_dataset_preprocessed.arff");
@@ -59,16 +64,20 @@ public class Main {
         classifiers.add(m5p);
         classifiers.add(multilayerPerceptron);
 
+        String[] header = {"", "RandomForest", "RandomTree", "SMOreg", "LinearRegression", "M5P", "MultilayerPerceptron"};
         String[][] results = {
-                {"Correlation coefficient:", ""},
-                {"Mean absolute error:", ""},
-                {"Root mean squared error:", ""},
-                {"Relative absolute error:", ""},
-                {"Root relative squared error:", ""},
-                {"Total Number of Instances:", ""},
-                {"Time taken:", ""}
+                {"Correlation coefficient:", "", "", "", "", "", ""},
+                {"Mean absolute error:", "", "", "", "", "", ""},
+                {"Root mean squared error:", "", "", "", "", "", ""},
+                {"Relative absolute error:", "", "", "", "", "", ""},
+                {"Root relative squared error:", "", "", "", "", "", ""},
+                {"Total Number of Instances:", "", "", "", "", "", ""},
+                {"Time taken:", "", "", "", "", "", ""}
         };
 
+        int index = 1;
+        double Score = Double.NEGATIVE_INFINITY;
+        Classifier bestClassifier = null;
 
         for (Classifier classifier : classifiers) {
             long startTime = System.currentTimeMillis();
@@ -77,17 +86,27 @@ public class Main {
             eval.crossValidateModel(classifier, standardizedData, 10, new Random(1));
             long endTime = System.currentTimeMillis();
             long timeTaken = endTime - startTime;
-            System.out.println("\nClassifier: " + classifier.getClass().getSimpleName());
-            System.out.println("=====================================");
-            results[0][1] = String.format("%.5f", eval.correlationCoefficient());
-            results[1][1] = String.format("%.5f", eval.meanAbsoluteError());
-            results[2][1] = String.format("%.5f", eval.rootMeanSquaredError());
-            results[3][1] = String.format("%.5f", eval.relativeAbsoluteError()) + "%";
-            results[4][1] = String.format("%.5f", eval.rootRelativeSquaredError()) + "%";
-            results[5][1] = String.valueOf((int) eval.numInstances());
-            results[6][1] = timeTaken + "ms";
-            System.out.println(AsciiTable.getTable(results));
-        }
+//            System.out.println("\nClassifier: " + classifier.getClass().getSimpleName());
+//            System.out.println("=====================================");
+            results[0][index] = String.format("%.5f", eval.correlationCoefficient());
+            results[1][index] = String.format("%.5f", eval.meanAbsoluteError());
+            results[2][index] = String.format("%.5f", eval.rootMeanSquaredError());
+            results[3][index] = String.format("%.5f", eval.relativeAbsoluteError()) + "%";
+            results[4][index] = String.format("%.5f", eval.rootRelativeSquaredError()) + "%";
+            results[5][index] = String.valueOf((int) eval.numInstances());
+            results[6][index] = timeTaken + " ms";
+            index++;
 
+            if (eval.correlationCoefficient() > Score) {
+                Score = eval.correlationCoefficient();
+                bestClassifier = classifier;
+            }
+        }
+//
+        System.out.println(AsciiTable.getTable(header, results));
+
+        System.out.println("=======================================================================");
+        System.out.println("Best Classifier: " + bestClassifier.getClass().getSimpleName());
+        System.out.println("Score: " + Score);
     }
 }
